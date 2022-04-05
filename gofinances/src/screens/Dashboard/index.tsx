bf7b67c85ca0758185918ce1fad8d04ada301528
Edésio Gonçalves <60361061+EdesioGoncalves@users.noreply.github.com>
2022-04-04 21:58:07 -0300

    Novo projeto
    Projeto de controle financeiro

---
+++ gofinances/src/screens/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { 
+    Container,
+    Text
+} from './styles';
+
+export function Dashboard(){
+    return(
+        <Container>
+            <Text>Dashboard</Text>
+        </Container>
+    )
+}
---
+++ gofinances/src/screens/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: #999;
+`;
+
+export const Text = styled.Text`
+    font-size: 20px;
+    color: #fff
+`;
